4ae7db6bd692fe8cd3519f522326e4edd1b37a5b
Your Name <you@example.com>
2018-02-25 14:53:28 +0300

    last

--- test.php
+++ test.php
@@ -1,60 +1,48 @@
 <?php
 	$dir = getcwd() . '/tests/';
 	$filelist = scandir($dir, 1);
-	if (isset($_POST['test_id']))
-	{	
+	if (isset($_POST['test_id'])) {	
 		$id = htmlspecialchars(stripslashes($_POST['test_id']));
 		
-	} elseif (isset($_GET['test_id'])&&(is_numeric($_GET['test_id'])))
-	{
+	} elseif (isset($_GET['test_id'])&&(is_numeric($_GET['test_id']))) {
 		$id = htmlspecialchars(stripslashes($_GET['test_id']))-1;
-	} 
-	else 
-	{
+	} else  {
 		die("Некорретные данные!");
 	}
 	
 	$id = (intval($id));
 	
-	if ($id <= (count($filelist)-3))
-	{
+	if ($id <= (count($filelist)-3)) {
 		$json = $dir . "$filelist[$id]";
 		$test = json_decode(file_get_contents($json), true);
-	if (isset($test['1']['textQwestion']))
-	{
-	if (isset($_POST['test_id']))
-	{	
-		if (isset($_POST['userAnswer']))
-		{
-			$userAnswer = $_POST['userAnswer'];
-			if(count($userAnswer) === count($test))	
-			{	
-				$result = 0;
-				foreach ($test as $key => $value)
-				{	
-					if ($value['correct'] == $userAnswer[$key])
-					{
-						$result++;
-					}
-				}
-				
-				echo "Ваш результат: $result правильных из " . count($userAnswer);
-				if ($result ===count($userAnswer)) {
-				$userName = $_POST['name'];
-				header('Location: http://university.netology.ru/u/avolvach/me/dz7/cert.php?name='.$userName);
-				}
-			} else {
-    			echo "Не все поля формы заполнены. Повторите ввод!";
-    		}
-    	} else {
-    		echo "Введите ответы!";
-    	}
-	}
-} else {
+		if (isset($test['1']['textQwestion'])) {
+			if (isset($_POST['test_id'])) {	
+				if (isset($_POST['userAnswer'])) {
+					$userAnswer = $_POST['userAnswer'];
+					if(count($userAnswer) === count($test))	{	
+						$result = 0;
+						foreach ($test as $key => $value) {	
+							if ($value['correct'] == $userAnswer[$key])	{
+								$result++;
+							}
+						}
+						echo "Ваш результат: $result правильных из " . count($userAnswer);
+						if ($result ===count($userAnswer)) {
+						$userName = $_POST['name'];
+						header('Location: http://university.netology.ru/u/avolvach/me/dz7/cert.php?name='.$userName);
+						}
+					} else {
+    					echo "Не все поля формы заполнены. Повторите ввод!";
+    				}
+    			} else {
+    				echo "Введите ответы!";
+    			}
+			}
+		} else {
 			http_response_code(404);
 			echo 'Cтраница не найдена!';
 			exit(1);
-}
+		}
 	} else {
 		die("Файл не существует!<p><a href=\"list.php\">Выбрать другой тест</a></p>");
 	}
